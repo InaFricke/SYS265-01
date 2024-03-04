b33535fddaed73cedcaafc2d5c7e20d0107eda16
InaFricke <ina.fricke@mymail.champlain.edu>
2024-03-04 16:21:54 +0000

    updated script

--- linux/ubuntu/secure-ssh.sh
+++ linux/ubuntu/secure-ssh.sh
@@ -1,7 +1,7 @@
 #secure-ssh.sh
 #author InaFricke
 #creates a new ssh user using $1 parameter
-
+#adds a public key from the local repo
 sudo useradd -m -d /home/${1}/ -s /bin/bash ${1}
 
 echo "new user created"
@@ -10,20 +10,27 @@ sudo mkdir /home/${1}/.ssh
 
 echo "directory created"
 
-#adds a public key from the local repo or curled from the remote repo
+cd ..
+
+cd ..
+
+cd ..
 
 sudo cp SYS265-01/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
 
-echo "public key copied to directory"
-
-#removes roots ability to ssh in
-
+echo "public key copied to hidden directory"
 
 sudo chmod 700 /home/$1/.ssh
 
+#only owner can read write and execute
+
 sudo chmod 600 /home/$1/.ssh/authorized_keys
 
+#only the  owner can read and write in the auth keys file
+
 sudo chown -R  ${1}:${1} /home/${1}/.ssh
+
+# changes the owner of directory to the $1 user 
 
 echo "access changed"
 
